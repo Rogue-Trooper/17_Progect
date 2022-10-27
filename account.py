79308d677ffae7284b8721a8a0f1635a4fec406d
ivanvikvik <ivanvikvik@gmail.com>
2022-10-27 21:50:06 +0300

    Add in Account constructor and destructor

--- account.py
+++ account.py
@@ -1,9 +1,21 @@
 class Account:
-    def init(self, id, balance, name, cofficient=0.01):
+
+    # def __init__(self):
+    #     self.id = 0
+    #     self.balance = 0
+    #     self.name = "no name"
+    #     self.cofficient = 0.001
+
+    # constructor with params
+    def __init__(self, id, balance, name, cofficient=0.01):
         self.id = id
         self.balance = balance
         self.name = name
         self.cofficient = cofficient
+
+    # destructor
+    def __del__(self):
+        pass
 
     def get_info(self):
         return (f"{self.id}: "
